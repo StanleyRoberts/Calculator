856443eba8f6c9f9578924a102ce3ff2e0feff26
Stanley Roberts <stanley.heronwood@gmail.com>
2019-11-20 17:56:52 +0000

    Added JUnit tests for NumStack

---
+++ branches/Model/test/testcalculator/TestNumStack.java
@@ -0,0 +1,44 @@
+package testcalculator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import calculator.EmptyStackException;
+import calculator.Entry;
+import calculator.Stack;
+import calculator.Symbol;
+
+import org.junit.Test;
+
+public class TestNumStack {
+
+  @Test
+  //Test 1
+  //Tests creating a new NumStack and pushing a float to it
+  public void testConstructorAndPush() {
+    NumStack stack = new NumStack();
+    stack.push(5);
+    stack.push(13.2);
+  }
+  
+  @Test
+  //Test 2
+  //Tests that pop correctly pops and returns floats from the stack
+  public void testPop() throws EmptyStackException {
+    NumStack stack = new NumStack();
+    stack.push(7);
+    float value = stack.pop();
+    assertEquals(7, value, 0);
+  }
+  
+  @Test
+  //Test 3
+  //Tests isEmpty method functions correctly
+  public void testIsEmpty() throws EmptyStackException {
+    NumStack stack = new NumStack();
+    assertTrue(stack.isEmpty());
+    stack.push(3);
+    stack.pop();
+    assertTrue(stack.isEmpty());
+  }
+}
