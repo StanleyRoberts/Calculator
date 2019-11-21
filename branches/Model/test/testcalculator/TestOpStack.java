1c92c5516038716979e12c998f476e5291680ae0
Stanley Roberts <stanley.heronwood@gmail.com>
2019-11-21 18:43:20 +0000

    Added the operator stack along with minor fixes to the JUnit test case and entry class

--- branches/Model/test/testcalculator/TestOpStack.java
+++ branches/Model/test/testcalculator/TestOpStack.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import calculator.EmptyStackException;
-import calculator.NumStack;
+import calculator.OpStack;
 import calculator.Symbol;
 
 import org.junit.Test;
