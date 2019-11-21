7b5cfddbfe210439f53f228e8a3fbeef3733235d
Stanley Roberts <stanley.heronwood@gmail.com>
2019-11-21 17:45:09 +0000

    JUnit tests for operator stack

---
+++ branches/Model/test/testcalculator/TestOpStack.java
@@ -0,0 +1,43 @@
+package testcalculator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import calculator.EmptyStackException;
+import calculator.NumStack;
+import calculator.Symbol;
+
+import org.junit.Test;
+
+public class TestOpStack {
+
+  @Test
+  //Test 1
+  //Tests creating a new NumStack and pushing a Symbol to it
+  public void testConstructorAndPush() {
+    OpStack stack = new OpStack();
+    stack.push(Symbol.LEFT_BRACKET);
+    stack.push(Symbol.PLUS);
+  }
+
+  @Test
+  //Test 2
+  //Tests that pop correctly pops and returns Symbols from the stack
+  public void testPop() throws EmptyStackException {
+    OpStack stack = new OpStack();
+    stack.push(Symbol.TIMES);
+    Symbol symbol = stack.pop();
+    assertEquals(Symbol.TIMES, symbol);
+  }
+
+  @Test
+  //Test 3
+  //Tests isEmpty method functions correctly
+  public void testIsEmpty() throws EmptyStackException {
+    OpStack stack = new OpStack();
+    assertTrue(stack.isEmpty());
+    stack.push(Symbol.RIGHT_BRACKET);
+    stack.pop();
+    assertTrue(stack.isEmpty());
+  }
+}
